cd80ed246af0bfdc9e5109a30e26cc7c04e5c3cc
larago <bingeye@foxmail.com>
2017-01-28 16:18:29 +0800

    monkey patch

--- monkey_patch.py
+++ monkey_patch.py
@@ -1,1 +1,15 @@
 # encoding=utf8
+
+import socket
+print socket.socket
+
+print 'after monkey patch'
+from gevent import monkey
+monkey.patch_socket()
+print socket.socket
+
+import select
+print select.select
+monkey.patch_select()
+print select.select
+
