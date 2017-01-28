909d81562f355de07bfe66130757869a7b59b2a9
larago <bingeye@foxmail.com>
2017-01-28 16:53:47 +0800

    set timeout for worker while async

---
+++ gevent_queue_timeout_unblock.py
@@ -0,0 +1,31 @@
+# encoding=utf8
+
+import gevent
+from gevent.queue import Queue, Empty
+
+tasks = Queue(maxsize=3)
+
+def worker(n):
+    try:
+        while True:
+            task = tasks.get(timeout=1)
+            print 'Worker %s got task %s' % (n, task)
+            gevent.sleep(0)
+    except Empty:
+        print 'Quiting time'
+
+def boss():
+    for i in xrange(1, 10):
+        tasks.put(i)
+    print 'Assigned all work in iteration 1'
+
+    for i in xrange(10, 20):
+        tasks.put(i)
+    print 'Assigned all work in iteration 2'
+
+gevent.joinall([
+    gevent.spawn(boss),
+    gevent.spawn(worker, 'steve'),
+    gevent.spawn(worker, 'john'),
+    gevent.spawn(worker, 'bob'),
+])
