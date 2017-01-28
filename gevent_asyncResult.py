a1af2937903fa0d7f903b88612c47c1e9524337d
larago <bingeye@foxmail.com>
2017-01-28 16:42:36 +0800

    asyncResult of gevent

---
+++ gevent_asyncResult.py
@@ -0,0 +1,18 @@
+# encoding=utf8
+
+import gevent
+from gevent.event import AsyncResult
+a = AsyncResult()
+
+def setter():
+    gevent.sleep(3)
+    a.set('Hello!')
+
+def waiter():
+    print 'this is', a.get()
+    print 'after'
+
+gevent.joinall([
+    gevent.spawn(setter),
+    gevent.spawn(waiter),
+])
